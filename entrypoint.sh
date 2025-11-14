3cbc51057014e6a27df828f0ffeebb83915b570f
mokira3d48 <arnoldmokira3d48@gmail.com>
2025-11-14 02:34:36 +0100

    Error system handler is added and the entrypoint bug is fixed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 source /env/bin/activate
 
-if [$1 == 'gunicorn']; then
-	exec gunicorn core.wsgi:application -b 0.0.0.0:8080
+if [ "$1" == "gunicorn" ]; then
+	gunicorn core.wsgi:application -b 0.0.0.0:8080
 else
-	exec manage runserver 0.0.0.0:8080
+	manage runserver 0.0.0.0:8080
 fi
 
--- server/core/exceptions.py
+++ server/core/exceptions.py
@@ -1,42 +1,50 @@
 import sys
 import traceback
 import logging
+import json
 
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+from rest_framework.exceptions import APIException
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
-from rest_framework import status
 
-#from apps.base.interfaces.views import exception_handler as base_exception_handler
+# from your_app.exceptions import exception_handler as your_app_exception_handler
+
 
 LOGGER = logging.getLogger(__name__)
-DEFAULT_RESPONSE_EXAMPLES = {
-    'error': _('An internal server error is occurrence!'),
-    'code': 'unknown',
-    }
 
 
-def print_error_traceback() -> None:
+class APIExceptionSerializer(serializers.Serializer):
+    detail = serializers.CharField(read_only=True)
+    code = serializers.CharField(read_only=True)
+
+
+def print_error_traceback():
     """Printing of details of the error."""
     exc_type, exc_value, exc_traceback = sys.exc_info()
     tb_obj = traceback.extract_tb(exc_traceback)
     for o in tb_obj:
-        LOGGER.error(" %16s %8d %64s" % (o.name, o.lineno, o.filename,))
+        LOGGER.error("%16s %8d %64s" % (o.name, o.lineno, o.filename,))
 
 
-def exception_handler_fn(exc, context) -> Response:
+def exception_handler_fn(exc, context):
     """Function of exception handling in API."""
     response = exception_handler(exc, context)
-    # response = base_exception_handler(response, exc, context)
-    if response is None:
-        # raise exc
-        LOGGER.error(str(exc))
+    # response = your_app_exception_handler(response, exc, context)
+
+    if settings.DEBUG:
         print_error_traceback()
-        return Response(
-            dict(
-                error=DEFAULT_RESPONSE_EXAMPLES['error'],
-                message=str(exc), code=DEFAULT_RESPONSE_EXAMPLES['code']
-                ),
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
+
+    if response is None:
+        if isinstance(exc, APIException):
+            if isinstance(exc.detail, (list, dict)):
+                exc.detail = json.dumps(exc.detail, indent=4)
+            else:
+                exc.detail = str(exc.detail)
+            response_ser = APIExceptionSerializer(instance=exc)
+            response = Response(response_ser.data, status=exc.status_code)
+
     return response
+
