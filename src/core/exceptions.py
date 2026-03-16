1248b3edc73c135e39d050995e627523a8a3bafd
Doctor Mokira <arnoldmokira3d48@gmail.com>
2026-03-16 13:45:10 +0100

    Refactor exception logging and response handling

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,19 +1,16 @@
 import sys
 import traceback
-import logging
 import json
 
+from loguru import logger
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from rest_framework.exceptions import APIException
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
 # from your_app.exceptions import exception_handler as your_app_exception_handler
-
-
-LOGGER = logging.getLogger(__name__)
 
 
 class APIExceptionSerializer(serializers.Serializer):
@@ -26,7 +23,7 @@ def print_error_traceback():
     exc_type, exc_value, exc_traceback = sys.exc_info()
     tb_obj = traceback.extract_tb(exc_traceback)
     for o in tb_obj:
-        LOGGER.error("%16s %8d %64s" % (o.name, o.lineno, o.filename,))
+        logger.error("%16s %8d %64s" % (o.name, o.lineno, o.filename,))
 
 
 def exception_handler_fn(exc, context):
@@ -43,8 +40,12 @@ def exception_handler_fn(exc, context):
                 exc.detail = json.dumps(exc.detail, indent=4)
             else:
                 exc.detail = str(exc.detail)
-            response_ser = APIExceptionSerializer(instance=exc)
-            response = Response(response_ser.data, status=exc.status_code)
+            response = Response(APIExceptionSerializer(instance=exc).data, status=exc.status_code)
+        else:
+            if getattr(settings, 'DEBUG') is True:
+                response = Response(
+                    APIExceptionSerializer(instance=exc).data,
+                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                )
 
     return response
-
