18426510f68bfcb8a972de66e51cb39b67103340
Doctor Mokira <arnoldmokira3d48@gmail.com>
2026-03-17 19:36:53 +0100

    Refactor API exception handling with custom classes

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,25 +1,35 @@
 import sys
 import traceback
+import logging
 import json
 
 from loguru import logger
 from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from rest_framework.exceptions import APIException
-from rest_framework import serializers, status
+from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import exception_handler
 
 # from your_app.exceptions import exception_handler as your_app_exception_handler
 
 
-class APIExceptionSerializer(serializers.Serializer):
-    detail = serializers.CharField(read_only=True)
+class CustomAPIException(APIException):
+
+    def __init__(self, detail=None, code=None):
+        super().__init__(detail, code)
+        self.code = code if code is not None else self.__class__.default_code
+
+
+class CustomAPIExceptionSerializer(serializers.Serializer):
+    detail = serializers.JSONField(read_only=True)
     code = serializers.CharField(read_only=True)
 
 
-def print_error_traceback():
-    """Printing of details of the error."""
+def print_error_traceback() -> None:
+    """
+    Printing of details of the error.
+    """
     exc_type, exc_value, exc_traceback = sys.exc_info()
     tb_obj = traceback.extract_tb(exc_traceback)
     for o in tb_obj:
@@ -33,21 +43,9 @@ def exception_handler_fn(exc, context):
 
     if settings.DEBUG:
         print_error_traceback()
-
+    # logger.info("exc: " + str(vars(exc)))
     if response is None:
-        if isinstance(exc, APIException):
-            if isinstance(exc.detail, (list, dict)):
-                exc.detail = json.dumps(exc.detail, indent=4)
-            else:
-                exc.detail = str(exc.detail)
-            response = Response(APIExceptionSerializer(instance=exc).data, status=exc.status_code)
-        else:
-            if getattr(settings, 'DEBUG') is True:
-                logger.error("{exception}: {message}".format(exception=exc.__class__.__name__, message=str(exc)))
-                api_excp = APIException(detail=str('\n'.join(m for m in exc.args)), code=exc.__class__.__name__)
-                response = Response(
-                    APIExceptionSerializer(instance=api_excp).data,
-                    status=status.HTTP_500_INTERNAL_SERVER_ERROR
-                )
-
+        exc = CustomAPIException({'errors': exc.args}, code=exc.__class__.__name__)
+        ser = CustomAPIExceptionSerializer(instance=exc)
+        return Response(ser.data, status=exc.status_code)
     return response
