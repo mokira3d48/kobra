9c82b46175be0567cc849756b9e746887d6a19f7
Doctor Mokira <arnoldmokira3d48@gmail.com>
2026-03-17 19:38:46 +0100

    Add custom API exception handler and serializers
    Implemented custom exception handling for API errors.

---
+++ api_exception_handler.py
@@ -0,0 +1,31 @@
+from typing import Any, Optional
+from rest_framework import serializers
+from rest_framework.exceptions import APIException
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class CustomAPIException(APIException):
+    
+    def __init__(self, detail: Optional[Any]=None, code: Optional[str]=None) -> None:
+        super().__init__(detail, code)
+        self.detail = detail if detail is not None else self.__class__.default_detail
+        self.code = code if code is not None else self.__class__.default_code
+
+
+class CustomAPIErrorSerializer(serializers.Serializer):
+    detail = serializers.JSONField(read_only=True)
+    code = serializers.CharField(read_only=True)
+
+
+class ValidationError(CustomAPIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+
+
+class NotFound(CustomAPIException):
+    status_code = status.HTTP_404_NOT_FOUND
+
+
+def exception_handler(response, exc, context) -> Response:
+    if isinstance(exc, CustomAPIException):
+        return Response(data=CustomAPIErrorSerializer(instance=exc).data, status=exc.status_code)
